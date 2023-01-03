4e788f98751fb9e6a6d7b317e17f34305ae0b89e
mizukihitosugi <mizuki.hitosugi1311@gmail.com>
2023-01-03 22:11:46 +0900

    [Finish] 1-17

--- assessment.js
+++ assessment.js
@@ -1,4 +1,30 @@
 'use strict';
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result-area');
+const tweetDivided = document.getElementById('tweet-area');
+
+assessmentButton.onclick = () => {
+  const userName = userNameInput.value;
+  if (userName.length === 0) {
+    // 名前が空の時は処理を終了する
+    return;
+  }
+
+  // 診断結果表示エリアの作成
+  const header = document.createElement('h3');
+  header.innerText = '診断結果';
+  resultDivided.appendChild(header);
+
+  const paragraph = document.createElement('p');
+  const result = assessment(userName);
+  paragraph.innerText = result;
+  resultDivided.appendChild(paragraph);
+
+  // TODO ツイートエリアの作成
+  tweetDivided.innerText = '';
+};
+
 const answers = [
   '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
   '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
@@ -20,13 +46,13 @@ const answers = [
 
 /**
  * 名前の文字列を渡すと診断結果を返す関数
- * @param {string} userName ユーザーの名前
+ * @param {string} userName ユーザの名前
  * @return {string} 診断結果
  */
 function assessment(userName) {
   // 全文字のコード番号を取得してそれを足し合わせる
   let sumOfCharCode = 0;
-  for (let i = 0; i <userName.length; i++) {
+  for (let i = 0; i < userName.length; i++) {
     sumOfCharCode = sumOfCharCode + userName.charCodeAt(i);
   }
 
@@ -38,6 +64,12 @@ function assessment(userName) {
   return result;
 }
 
+// テストコード
+console.assert(
+  assessment('太郎') ===
+  '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+  '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
+)
 console.assert(
   assessment('太郎') === assessment('太郎'),
   '診断結果の文言の特定の部分を名前に置き換える処理が正しくありません。'
